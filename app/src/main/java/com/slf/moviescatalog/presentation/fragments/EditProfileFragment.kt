a40e346b4cd27aced5d26115c5c5617fc45ba874
Surya Luqman Fathulloh <suryaluqman01@gmail.com>
2024-05-31 06:59:55 +0700

    fix: layout width image

--- app/src/main/java/com/slf/moviescatalog/presentation/fragments/EditProfileFragment.kt
+++ app/src/main/java/com/slf/moviescatalog/presentation/fragments/EditProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.slf.moviescatalog.presentation.fragments
 
+import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -17,6 +18,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
@@ -48,8 +51,9 @@ class EditProfileFragment : Fragment() {
     private var selectedBitmap: Bitmap? = null
 
     companion object {
-        private const val REQUEST_CODE_IMAGE_PICKER = 2
-        private const val REQUEST_CODE_IMAGE_CAPTURE = 4
+        private const val REQUEST_IMAGE_CAPTURE = 1
+        private const val SELECT_PICTURE = 2
+        private const val CAMERA_REQUEST_CODE = 100
         private const val REQUEST_CODE_PERMISSION = 3
     }
 
@@ -76,30 +80,28 @@ class EditProfileFragment : Fragment() {
             bornedit.setOnClickListener { showDatePickerDialog() }
 
             photoedit.setOnClickListener {
-                if (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.READ_EXTERNAL_STORAGE)
-                    == PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA)
-                    == PackageManager.PERMISSION_GRANTED) {
-                    openImageSourceChooser()
-                } else {
-                    requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE, android.Manifest.permission.CAMERA), REQUEST_CODE_PERMISSION)
-                }
+                selectImage()
             }
         }
     }
 
-    private fun openImageSourceChooser() {
+    private fun selectImage() {
         val options = arrayOf<CharSequence>("Take Photo", "Choose from Gallery", "Cancel")
-        val builder: android.app.AlertDialog.Builder = android.app.AlertDialog.Builder(requireContext())
-        builder.setTitle("Select Option")
+        val builder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        builder.setTitle("Add Photo!")
+
         builder.setItems(options) { dialog, item ->
             when {
                 options[item] == "Take Photo" -> {
-                    val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                    startActivityForResult(takePictureIntent, REQUEST_CODE_IMAGE_CAPTURE)
+                    if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
+                    } else {
+                        dispatchTakePictureIntent()
+                    }
                 }
                 options[item] == "Choose from Gallery" -> {
-                    val pickPhotoIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-                    startActivityForResult(pickPhotoIntent, REQUEST_CODE_IMAGE_PICKER)
+                    selectImageFromGallery()
                 }
                 options[item] == "Cancel" -> {
                     dialog.dismiss()
@@ -109,12 +111,48 @@ class EditProfileFragment : Fragment() {
         builder.show()
     }
 
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(requireActivity().packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    private fun selectImageFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, SELECT_PICTURE)
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                dispatchTakePictureIntent()
+            } else {
+                Toast.makeText(requireContext(), "Permission to use camera was denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
     @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK) {
             when (requestCode) {
-                REQUEST_CODE_IMAGE_PICKER -> {
+                REQUEST_IMAGE_CAPTURE -> {
+                    val imageBitmap = data?.extras?.get("data") as Bitmap
+                    selectedBitmap = imageBitmap
+                    binding.photoedit.setImageBitmap(selectedBitmap)
+                    selectedImage = saveImageToInternalStorage(imageBitmap)
+                }
+                SELECT_PICTURE -> {
                     selectedImage = data?.data
                     context?.let {
                         try {
@@ -134,15 +172,6 @@ class EditProfileFragment : Fragment() {
                         } catch (e: Exception) {
                             e.printStackTrace()
                         }
-                    }
-                }
-                REQUEST_CODE_IMAGE_CAPTURE -> {
-                    val imageBitmap = data?.extras?.get("data") as? Bitmap
-                    imageBitmap?.let {
-                        selectedBitmap = it
-                        binding.photoedit.setImageBitmap(it)
-                        // Save the image to internal storage
-                        selectedImage = saveImageToInternalStorage(it)
                     }
                 }
             }
@@ -175,18 +204,6 @@ class EditProfileFragment : Fragment() {
             e.printStackTrace()
             null
         }
-    }
-
-    @Deprecated("Deprecated in Java")
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if (requestCode == REQUEST_CODE_PERMISSION) {
-            if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
-                openImageSourceChooser()
-            } else {
-                Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show()
-            }
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     override fun onDestroyView() {
@@ -246,48 +263,27 @@ class EditProfileFragment : Fragment() {
                 Nameedit.text.isNullOrEmpty() -> Nameedit.error = "Fill the name"
                 Emailedit.text.isNullOrEmpty() -> Emailedit.error = "Fill the email"
                 !Validation.isValidEmail(Emailedit.text.toString()) -> Emailedit.error = "Enter a valid email"
-                bornedit.text.isNullOrEmpty() -> bornedit.error = "Fill the age"
+                bornedit.text.isNullOrEmpty() -> bornedit.error = "Fill the date of birth"
                 addressedit.text.isNullOrEmpty() -> addressedit.error = "Fill the address"
                 else -> {
-                    userViewModel.user.observe(viewLifecycleOwner) { user ->
-                        val newData = User(
-                            user.id,
-                            Nameedit.text.toString(),
-                            selectedImage?.toString() ?: user.profilePhoto,
-                            Emailedit.text.toString(),
-                            bornedit.text.toString(),
-                            addressedit.text.toString(),
-                            user.username,
-                            user.password
-                        )
-
-                        // Debug log to check the new profile photo URI
-                        Log.d("EditProfileFragment", "Saving profile photo URI: ${newData.profilePhoto}")
-
-                        lifecycleScope.launch(Dispatchers.IO) {
-                            userRepository.saveUser(newData)
-                            runBlocking(Dispatchers.Main) {
-                                Toast.makeText(requireContext(), "Edit Profile Success", Toast.LENGTH_SHORT).show()
-                                activity?.let {
-                                    val intent = Intent(it, MainActivity::class.java)
-                                    it.startActivity(intent)
-                                }
-                            }
+                    val user = User(
+                        id = 0,
+                        name = Nameedit.text.toString(),
+                        email = Emailedit.text.toString(),
+                        born = bornedit.text.toString(),
+                        address = addressedit.text.toString(),
+                        username = shared.getString(Constant.USERNAME).toString(),
+                        password = shared.getString(Constant.PASSWORD).toString(),
+                        profilePhoto = selectedImage.toString()
+                    )
+                    lifecycleScope.launch(Dispatchers.IO) {
+                        userRepository.saveUser(user)
+                        runBlocking(Dispatchers.Main) {
+                            Snackbar.make(binding.root, "Profile saved successfully", Snackbar.LENGTH_SHORT).setTextColor(Color.WHITE).setBackgroundTint(Color.BLUE).show()
                         }
                     }
                 }
             }
         }
-    }
-
-    fun onSNACK(view: View) {
-        val snackbar = Snackbar.make(view, "Edit Profile Success", Snackbar.LENGTH_LONG).setAction("Action", null)
-        snackbar.setActionTextColor(Color.BLACK)
-        val snackbarView = snackbar.view
-        snackbarView.setBackgroundColor(Color.LTGRAY)
-        val textView = snackbarView.findViewById(com.google.android.material.R.id.snackbar_text) as TextView
-        textView.setTextColor(Color.WHITE)
-        textView.textSize = 28f
-        snackbar.show()
     }
 }
